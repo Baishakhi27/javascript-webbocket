445a5fad8708dccf93cfcd0ad1eac1e4cf9dbc72
Baishakhi27 <152278178+Baishakhi27@users.noreply.github.com>
2023-12-05 12:44:38 +0000

    5th dec ,all data added

---
+++ 01-basics/array.js
@@ -0,0 +1,91 @@
+//  const arrayName=['name1','name2','name3']
+// console.log(arrayName);
+
+// const myArray=new Array('saneay','bbsr',2,3,4);
+// console.log(myArray);
+
+// const car=[]
+// car[0]="TATA";
+// car[1]="BMW";
+// car[2]="HYUNDAI";
+// console.log(car);
+
+
+// const myArray=[1,2,3,4,5,6,7,8]
+
+//  //Array indexing starts with 0.
+// console.log(myArray[4]);
+
+//  //ARRAY methods
+// console.log(array__prot0__);
+
+// //ARRAY.prototype.push()
+// console.log(myArray.push(9));
+// console.log(myArray);//it adds a new element in the last of existing array.
+
+// //array.prototype.pop()
+// console.log(myArray.pop());
+// console.log(myArray);//it will remove a element from the last index of array
+
+// //ARRAY.prototype.unshift()
+// console.log(myArray.unshift(86));
+// console.log(myArray);//it will add a new element from the first index of array.
+
+// //ARRAY.prototype.shift()
+// console.log(myArray.shift());
+// console.log(myArray);//it will remove an element from the first index of array.
+
+// //ARRAY.prototype.includes()
+// console.log(myArray.includes(6));//it will check whether the element  is present or not in the array,if the element is present it returns true otherwise false.
+
+// //ARRAY.prototype.indexof()
+// console.log(myArray.indexOf(10)); //if the element will be present in the ARRAY then it will return the index value of that element otherwise it will return -1.
+
+// //ARRAY.PROTOTYPE.JOIN ()
+//  console.log(myArray);
+// const demoArray=myArray.join()
+//  console.log(demoArray);
+//  console.log(typeof demoArray);
+
+// //slice and splice method
+// const myArray=[1,2,3,4,5]
+// console.log(myArray);
+// const myNewArray=myArray.slice(1,4);
+// console.log(myNewArray);
+// console.log(myArray);
+
+// const myArray=[1,2,3,4,5]
+// console.log(myArray);
+// const myNewArray2=myArray.splice(1,4);
+// console.log(myNewArray2);
+// console.log(myArray);
+
+//ARRAY.PROTOTYPE.CONCAT()
+// const marvel_hero=['thor','ironman','spiderman','captainAmerica'];
+// const dc_hero=['batman','superman','flash'];
+// const newArray=marvel_hero.concat(dc_hero);
+// console.log(newArray);
+
+// concatenation,copy,applied method
+// const marvel_hero=['thor','ironman','spiderman','captainAmerica'];
+// const dc_hero=['batman','superman','flash'];
+// const newArray=[...marvel_hero,...dc_hero,"anjali"];
+// console.log(newArray);
+
+//Array.prototype.flat()
+// const anotherArray=[1,2,3,[4,5,6],[7,8,9],[10,11,12,[13,14,15,16]]];
+// const simplifiedArray=anotherArray.flat(Infinity);
+// console.log(simplifiedArray);
+
+//Array.prototype.isArray()
+// const marvel_hero=['thor','ironman','spiderman','captainAmerica'];
+// const dc_hero=['batman','superman','flash'];
+// console.log(Array.isArray(dc_hero));
+
+// example of isArray
+// const arrayNum = [1,2,3,4,5];
+// console.log(arrayNum);
+// console.log(Array.isArray(arrayNum));
+
+//Array.prototype.from()
+//console.log(Array.from("Bhubaneswar"));
